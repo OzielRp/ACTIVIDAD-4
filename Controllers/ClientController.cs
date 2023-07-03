dda45bc6fd6613743e441b211e2c48e4723eb30b
OzielRp <137758899+OzielRp@users.noreply.github.com>
2023-07-03 00:24:44 -0600

    Add files via upload

---
+++ Controllers/ClientController.cs
@@ -0,0 +1,75 @@
+
+using Microsoft.AspNetCore.Mvc;
+using BankAPI.Data;
+using BankAPI.Data.BankModels;
+
+namespace BankAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ClientController : ControllerBase
+{
+    private readonly BankContext _context;
+    public ClientController(BankContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public IEnumerable<Client> Get()
+    {
+        return _context.Clients.ToList();
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<Client> GetById(int id)
+    {
+        var client = _context.Clients.Find(id);
+
+        if (client is null)
+            return NotFound();
+
+        return client;   
+    }
+
+    [HttpPost]
+    public IActionResult Create(Client client)
+    {
+        _context.Clients.Add(client);
+        _context.SaveChanges();
+
+        return CreatedAtAction(nameof(GetById), new { id = client.Id}, client);
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult Update(int id, Client client)
+    {
+        if (id != client.Id)
+            return BadRequest();
+        var existingClient = _context.Clients.Find(id);
+        if (existingClient is null)
+            return NotFound();
+
+        existingClient.Name = client.Name;
+        existingClient.PhoneNumber = client.PhoneNumber;
+        existingClient.Email = client.Email;
+
+        _context.SaveChanges();        
+
+        return NoContent();
+
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        var existingClient = _context.Clients.Find(id);
+        if (existingClient is null)
+            return NotFound();
+        _context.Clients.Remove(existingClient);
+        _context.SaveChanges();
+
+        return Ok();
+    }
+
+}
---
+++ Data/BankContext.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using BankAPI.Data.BankModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace BankAPI.Data;
+
+public partial class BankContext : DbContext
+{
+    public BankContext()
+    {
+    }
+
+    public BankContext(DbContextOptions<BankContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Account> Accounts { get; set; }
+
+    public virtual DbSet<AccountType> AccountTypes { get; set; }
+
+    public virtual DbSet<BankTransaction> BankTransactions { get; set; }
+
+    public virtual DbSet<Client> Clients { get; set; }
+
+    public virtual DbSet<TransactionType> TransactionTypes { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Account>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Account__3214EC2748787EF1");
+
+            entity.ToTable("Account");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.Balance).HasColumnType("decimal(10, 2)");
+            entity.Property(e => e.ClientId).HasColumnName("ClientID");
+            entity.Property(e => e.RegDate)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+
+            entity.HasOne(d => d.AccountTypeNavigation).WithMany(p => p.Accounts)
+                .HasForeignKey(d => d.AccountType)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Account__Account__534D60F1");
+
+            entity.HasOne(d => d.Client).WithMany(p => p.Accounts)
+                .HasForeignKey(d => d.ClientId)
+                .HasConstraintName("FK__Account__ClientI__5441852A");
+        });
+
+        modelBuilder.Entity<AccountType>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__AccountT__3214EC27F4882509");
+
+            entity.ToTable("AccountType");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.Name)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.RegDate)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+        });
+
+        modelBuilder.Entity<BankTransaction>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__BankTran__3214EC271D23E51F");
+
+            entity.ToTable("BankTransaction");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.AccountId).HasColumnName("AccountID");
+            entity.Property(e => e.Amount).HasColumnType("decimal(10, 2)");
+            entity.Property(e => e.RegDate)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+
+            entity.HasOne(d => d.Account).WithMany(p => p.BankTransactions)
+                .HasForeignKey(d => d.AccountId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__BankTrans__Accou__6A30C649");
+
+            entity.HasOne(d => d.TransactionTypeNavigation).WithMany(p => p.BankTransactions)
+                .HasForeignKey(d => d.TransactionType)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__BankTrans__Trans__6B24EA82");
+        });
+
+        modelBuilder.Entity<Client>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Client__3214EC277B0EB0CE");
+
+            entity.ToTable("Client", tb =>
+                {
+                    tb.HasTrigger("ClientAfterInsert");
+                    tb.HasTrigger("ClientInsteadOfDelete");
+                });
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.Email)
+                .HasMaxLength(200)
+                .IsUnicode(false);
+            entity.Property(e => e.Name)
+                .HasMaxLength(200)
+                .IsUnicode(false);
+            entity.Property(e => e.PhoneNumber)
+                .HasMaxLength(200)
+                .IsUnicode(false);
+            entity.Property(e => e.RegDate)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+        });
+
+        modelBuilder.Entity<TransactionType>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Transati__3214EC270E9379E6");
+
+            entity.ToTable("TransactionType");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.Name)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.RegDate)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
